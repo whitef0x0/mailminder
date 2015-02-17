5e40218fb0afc3f6ac82988691c0b06ce7381ca8
David Baldwynn <shorerydr10@live.com>
2015-02-16 18:48:22 -0800

    added web app files to /webapp branch

--- mailminder.js
+++ mailminder.js
@@ -1,4 +1,4 @@
-var argv = require('minimist')(process.argv.slice(2));
+var startStopDaemon = require('start-stop-daemon');
 
 var mail = require('./lib/send_mail'),
 	CronJob = require('cron').CronJob,
@@ -6,31 +6,34 @@ var mail = require('./lib/send_mail'),
 	fs = require('fs');
 
 //Default Schedule is every Friday at 6PM
-var default_schedule = '06 00 06 * 5',
+var default_schedule = '00 18 * * 5',
     default_timezone = "America/Los_Angeles";
 
 
-if( argv._[0] === 'start' && argv.file){
-	var cron_tab = default_schedule;
-	var tz = default_timezone;
+startStopDaemon(function(){
+var argv = require('minimist')(process.argv.slice(2));
+	if(argv.file){
+		var cron_tab = default_schedule;
+		var tz = default_timezone;
 
 
-	var email_file = argv.file;
+		var email_file = argv.file;
 
-	if( argv.tz ) timezone = argv.tz;
-	if( argv.interval ) cron_tab = interval;
+		if( argv.tz ) timezone = argv.tz;
+		if( argv.interval ) cron_tab = interval;
 
-	var job = new CronJob({
-	  cronTime: '* * * * *', //'00 18 * * 5' ,
-	  onTick: function() {
-	    // Runs every Friday at 6:00 PM. 
-		mail.send_reminder(email_file);
-	  },
-	  start: false,
-	  timeZone: tz
-	});
+		var job = new CronJob({
+		  cronTime: cron_tab,
+		  onTick: function() {
+		    // Runs every time the cronjob is called 
+			mail.send_reminder(email_file);
+		  },
+		  start: false,
+		  timeZone: tz
+		});
 
-	job.start()
-	// require('daemon')();
-	// console.log(process.pid);
-}
+		job.start()
+	}
+}).on('start', function(){
+	this.console.write('Starting mailminder at '+ new Date() + '\n');
+});
